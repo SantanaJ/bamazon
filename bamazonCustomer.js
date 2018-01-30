76bbcb0485540428e6bcee43bdd8933558b49617
Jasmine Santana <jasmine@Jasmines-MacBook-Pro.local>
2018-01-29 22:10:56 -0500

    update

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,18 +1,90 @@
 var mysql = require("mysql");
+var inquirer = require("inquirer");
+require("console.table");
 
 var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
+    host: "localhost",
+    port: 3306,
 
-  // Your username
-  user: "root",
+    // Your username
+    user: "root",
 
-  // Your password
-  password: "",
-  database: "bamazon"
+    // Your password
+    password: "",
+    database: "bamazon"
 });
 
-connection.connect(function(err) {
-  if (err) throw err;
-  console.log("connected as id " + connection.threadId + "\n");
+connection.connect(function (err) {
+    if (err) throw err {
+        console.log("connected as id " + connection.threadId + "\n");
+    }
+    pullProducts();
 });
+
+function pullProduct() {
+    connection.query("SELECT * FROM products", function (err, res) {
+        if (err) throw err;
+        console.table(res);
+        requestItem(res);
+    });
+}
+
+function requestItem(inventory) {
+    inquirer
+        .prompt([{
+            type: "input",
+            name: "choice",
+            message: "What is it you are looking for?"
+        }])
+        .then(function (val) {
+            var idCheck = parseInt(val.choice);
+            var product = searchInventory(idCheck, inventory);
+
+            if (products) {
+                requestQuantity(product);
+            } else {
+                pullProduct();
+            }
+        });
+}
+
+function requestQuantity() {
+    inquirer
+        .prompt([{
+                type: "input",
+                name: "quantity",
+                message: "How do you want to order?"
+            }
+
+        ])
+
+        .then(function (val) {
+            var quantity = parseInt(val.quantity);
+
+            if (quantity > product.stock_quantity) {
+                pullProducts();
+            } else {
+                completePurchase(product, quantity);
+            }
+        });
+    }
+function completePurchase(product,quantity){
+connection.query(
+    "UPDATE products SET stock_quantity = stock_quantity -? WHERE item_id = ?",
+    [quantity, product.item_id],
+    function(err,res){
+        console.log("\nYour purchase " + quantity + ""+ product.product_name);
+        pullProducts();
+    }
+);
+}
+
+function searchInventory(idCheck, inventory){
+    for(var i = 0; i < inventory.length; i++){
+        if(inventory[i].item_id === idCheck){
+            return inventory [i];
+        }
+    }
+    return null;
+}
+
